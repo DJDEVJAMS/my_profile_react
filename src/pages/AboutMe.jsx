e5afdee5b5a574082ba04e95c2941b11b11f1f4b
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-31 15:54:24 -0500

    updating image routing

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe() {
         <section className="container mx-auto p-4">
             <h2 className="text-2xl font-bold mb-4">About Me</h2>
             <img src="./src/assets/images/IMG_4160.PNG" alt="Alex Brown" className="rounded-full w-32 h-32 mb-4" />
-            <p>Hello! I’m a full-stack developer with a passion for creating web applications. I'm a proud father and a loving husband. I am passionate about expanding my knowledge about technologies and how they impact the world. In my spare time I'm also a DJ. </p>
+            <p>Hello! I’m a dedicated full-stack developer with a passion for creating innovative web applications. Beyond coding, I’m a proud father and a loving husband, always eager to learn more about how technology shapes our world. In my spare time, I enjoy spinning tracks as a DJ and exploring new destinations with my family.</p>
         </section>
     );
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,13 +8,13 @@ const Portfolio = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Project 
           title="City Event Tracker" 
-          image="./src/assets/images/city_eventtracker.jpg" 
+          image="../src/assets/images/city_eventtracker.png" 
           link="https://djdevjams.github.io/city_eventtracker/" 
           repo="https://github.com/DJDEVJAMS/city_eventtracker"
         />
          <Project 
           title="Bonfire" 
-          image="./src/assets/images/bonfire.jpg" 
+          image="./src/assets/images/bonfire.png" 
           link="https://bonfire-lgkj.onrender.com/" 
           repo="https://github.com/your-repo"
         />
