40a28bf096724afb031cadd97785c101a1238a10
DJDEVJAMS <alex.art.brown@gmail.com>
2024-08-31 17:18:09 -0500

    updating image routing

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -4,7 +4,7 @@ function AboutMe() {
     return (
         <section className="container mx-auto p-4">
             <h2 className="text-2xl font-bold mb-4">About Me</h2>
-            <img src="./src/assets/images/IMG_4160.PNG" alt="Alex Brown" className="rounded-full w-32 h-32 mb-4" />
+            <img src="../../src/assets/images/IMG_4160.PNG" alt="Alex Brown" className="rounded-full w-32 h-32 mb-4" />
             <p>Hello! I’m a dedicated full-stack developer with a passion for creating innovative web applications. Beyond coding, I’m a proud father and a loving husband, always eager to learn more about how technology shapes our world. In my spare time, I enjoy spinning tracks as a DJ and exploring new destinations with my family.</p>
         </section>
     );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ function Resume() {
     return (
         <div className="container mx-auto p-4">
             <h2 className="text-2xl font-bold mb-4">Resume</h2>
-            <a href=".src/assets/docs/alexb_resume_2024.pdf" download className="text-blue-500 underline">Download My Resume</a>
+            <a href="../../src/assets/docs/alexb_resume_2024.pdf" download className="text-blue-500 underline">Download My Resume</a>
             <h3 className="text-xl font-bold mt-4">Proficiencies</h3>
             <ul className="list-disc pl-5">
                 <li>JavaScript</li>
